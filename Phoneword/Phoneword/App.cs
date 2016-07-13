54d14d7b45be36fcfc59941f44ee3934b6a80dfa
Brunera <lpbruno12@gmail.com>
2016-07-13 20:08:00 -0300

    First Deploy

--- Phoneword/Phoneword/App.cs
+++ Phoneword/Phoneword/App.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Xamarin.Forms;
 
 namespace Phoneword
@@ -11,20 +7,7 @@ namespace Phoneword
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new Phoneword.MainPage();
         }
 
         protected override void OnStart()
